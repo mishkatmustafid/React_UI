9d19b50965223971fd34b34651c899edb2b4eb66
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-03-17 15:05:06 +0600

    Index page created

---
+++ landing-page/src/pages/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}

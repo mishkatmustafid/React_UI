320a9f23655f3616b08e1c74f54e442a2af21037
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-03-18 18:39:55 +0600

    Homecard completed in card.jsx

--- landing-page/src/shared/card.jsx
+++ landing-page/src/shared/card.jsx
@@ -15,5 +15,22 @@ import {
 import "../static/css/card.css";
 
 export function HomeCard(props) {
-  return <MDBBtn rounded color></MDBBtn>;
+  return (
+    <MDBBtn
+      rounded
+      color='amber'
+      className={`mb-3 mt-4 mx-2 btn-block ${props.className}`}
+      href={props.url}
+      onClick={props.onClick}
+      style={props.style}
+    >
+      <MDBCardBody className='card-background px-lg-4 px-md-2 px-5'>
+        <div className='d-flex justify-content-center'>
+          <MDBIcon icon={props.icon} size='5x' style={{ color: "fff" }} />
+        </div>
+        <div className='p-3 m-3 text-center fw-bold'>{props.children}</div>
+        <h5>{props.name}</h5>
+      </MDBCardBody>
+    </MDBBtn>
+  );
 }

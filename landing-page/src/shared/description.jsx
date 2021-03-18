f892398dfac787d8560cf79b06a441bedbda7ad1
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-03-18 20:16:37 +0600

    Description modified

--- landing-page/src/shared/description.jsx
+++ landing-page/src/shared/description.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-
+import "../index.css";
 import LawEduThumb from "../static/img/askRiashad_icon_Study_Support.png";
 import TexThumb from "../static/img/askRiashad_icon_Incometax.png";
 import LawSupprtThumb from "../static/img/askRiashad_icon_Legal_Support.png";
@@ -31,18 +31,12 @@ export default class Description extends Component {
           >
             <h2 className='text-warning py-2'>আইন শিক্ষা</h2>
             <div className='text-justify'>
-              <p className='m-0'>
-                দেশের সেরা বিশ্ববিদ্যালয়ের সেরা শিক্ষকদের নোটস, লেকচার, গাইড{" "}
-              </p>
-              <p className='m-0'>
-                পৃথিবীর সর্ববৃহৎ বাংলাদেশী আইনের এমসিকিউ প্রশ্ন ব্যাংক{" "}
-              </p>
-              <p className='m-0'>রচনামূলক প্রশ্ন ব্যাংক, এবং উত্তর </p>
-              <p className='m-0'>
-                আনলিমিটেড বিষয় ভিত্তিক, কিংবা চ্যাপ্টার ভিত্তিক মডেল টেস্ট
-              </p>
-              <p className='m-0'>
-                রিভিশনের উন্নতি মনিটর করার জন্য আকর্ষণীয় ইনফোগ্রাফিক গাইড{" "}
+              <p className='m-0' style={{ fontSize: "23px" }}>
+                দেশের সেরা বিশ্ববিদ্যালয়ের সেরা শিক্ষকদের নোটস, লেকচার, গাইড
+                পৃথিবীর সর্ববৃহৎ বাংলাদেশী আইনের এমসিকিউ প্রশ্ন ব্যাংক রচনামূলক
+                প্রশ্ন ব্যাংক, এবং উত্তর আনলিমিটেড বিষয় ভিত্তিক, কিংবা চ্যাপ্টার
+                ভিত্তিক মডেল টেস্ট রিভিশনের উন্নতি মনিটর করার জন্য আকর্ষণীয়
+                ইনফোগ্রাফিক গাইড{" "}
               </p>
             </div>
           </MDBCol>
@@ -55,15 +49,11 @@ export default class Description extends Component {
           >
             <h2 className='text-warning py-2'>আয়কর রিটার্ন </h2>
             <div className='text-justify text-right'>
-              <p className='m-0'>
-                পূর্ণাঙ্গ আয়কর প্রস্তুত, হিসাব ও দাখিল করার সফটওয়্যার{" "}
-              </p>
-              <p className='m-0'>
-                ঝামেলাহীন স্বয়ংক্রিয় ভাবে প্রদেয় ট্যাক্স নির্ণয়
-              </p>
-              <p className='m-0'>পূর্ববর্তী আয়কর বছরের তথ্য সংরক্ষণ </p>
-              <p className='m-0'>
-                ব্যাক্তি, প্রতিষ্ঠান বা আয়কর আইনজীবী সকলের জন্য আনলিমিটেড এক্সেস
+              <p className='m-0' style={{ fontSize: "23px" }}>
+                পূর্ণাঙ্গ আয়কর প্রস্তুত, হিসাব ও দাখিল করার সফটওয়্যার ঝামেলাহীন
+                স্বয়ংক্রিয় ভাবে প্রদেয় ট্যাক্স নির্ণয় পূর্ববর্তী আয়কর বছরের তথ্য
+                সংরক্ষণ ব্যাক্তি, প্রতিষ্ঠান বা আয়কর আইনজীবী সকলের জন্য
+                আনলিমিটেড এক্সেস
               </p>
             </div>
           </MDBCol>
@@ -98,17 +88,12 @@ export default class Description extends Component {
           >
             <h2 className='text-warning py-2'>আইনী পরামর্শ </h2>
             <div className='text-justify '>
-              <p className='m-0'>
+              <p className='m-0' style={{ fontSize: "23px" }}>
                 পৃথিবীর সর্ব বৃহৎ বাংলাদেশি ফৌজদারি ও দেওয়ানি মামলা সংক্রান্ত
-                ড্রাফট ব্যাংক
-              </p>
-              <p className='m-0'>বিভিন্ন দলিল সম্পাদনের স্বয়ংক্রিয় সফটওয়্যার</p>
-              <p className='m-0'>
-                দেশের বিভিন্ন শহরে থাকা আইনজীবীদের মাধ্যমে আইনী পরামর্শ
-              </p>
-              <p className='m-0'>
-                আইনজীবী বা সাধারণ ব্যাক্তিদের জন্য সিভিল অথবা ক্রিমিনাল ড্রাফটিং
-                এর অসংখ্য নমুনা
+                ড্রাফট ব্যাংক বিভিন্ন দলিল সম্পাদনের স্বয়ংক্রিয় সফটওয়্যার দেশের
+                বিভিন্ন শহরে থাকা আইনজীবীদের মাধ্যমে আইনী পরামর্শ আইনজীবী বা
+                সাধারণ ব্যাক্তিদের জন্য সিভিল অথবা ক্রিমিনাল ড্রাফটিং এর অসংখ্য
+                নমুনা
               </p>
             </div>
           </MDBCol>
